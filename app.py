22737ac4b508288da5cd6cf8363acfd883f06d03
MrCic3ro <38115321+ciceromayk@users.noreply.github.com>
2026-01-30 13:55:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,12 @@ import streamlit as st
 from ui.dashboard import dashboard_view
 from ui.projeto import projeto_view
 
-
 st.set_page_config(page_title="RAIO X DE PROJETOS", layout="wide")
-
 
 st.sidebar.title("RAIO X DE PROJETOS")
 menu = st.sidebar.radio("MENU", ["Dashboard", "Projeto"])
 
-
 if menu == "Dashboard":
-dashboard_view()
+    dashboard_view()
 elif menu == "Projeto":
-projeto_view()
+    projeto_view()
